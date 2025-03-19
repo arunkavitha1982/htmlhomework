60cffe81fbf2b0a1d0be7e0690026f2efec1bfce
arunkavitha1982 <arunkumar.msg1982@gmail.com>
2025-03-19 17:47:48 +0530

    unwanted files deleted and update my portfolio

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
